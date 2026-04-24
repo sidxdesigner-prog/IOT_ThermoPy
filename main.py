4b6e2904a5141632a12d204894abbc7828b14989
Sid Laser <sidxdesigner@gmail.com>
2026-04-24 10:30:47 -0300

    main reajustado

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
 import time, json, random
 
-temperatura_Celsius = [22, 24, 31, 33, 29, 26, 17, 11, 35, 14, 10, 25]
-event_Celcius = ["dia_quente", "dia_frio"]
+temperatura_iniciais = [18, 19, 20, 34, 35, 36]
+temperatura = 28
+tendencia = 1
+potencia = 2.3
+clima = ["dia_quente", "dia_frio"]
 timestamp = time.time()
-ventilador = False
-aquecedor = False
-ativar_Ventilador = 30
-desativar_Ventilador = 27
-ativar_Aquecedor = 18
-desativar_Aquecedor = 21
-max_Temperatura = -100
-min_Temperatura = 100
-ativados_Ventilador = 0
-ativados_Aquecedor = 0
-looping = 0
-conteudo = []
-
-def sortear():
-    for i in range(5):
-        timetime = timestamp + (60 * i)
-        temp = random.choice(temperatura_Celsius)
-        conteudo.append({"timestamp": timetime, "temperatura": temp})
-    with open("sensor.json", "w+", encoding="utf-8") as temperatura_file_Out:
-        json.dump(conteudo, temperatura_file_Out, ensure_ascii = False, indent = 2)
-
-#def diaQuente():
-
-#def diaFrio():
-
-#def verificar_temperatura():
+dev_1 = "Alexandre"
+dev_2 = "Pedro"
+gerente = "SID_Devops"
 
 
-#with open("sensor.json", "r", encoding="utf-8") as temperatura_file:
-   # conteudo = json.load(temperatura_file)
-sortear()
+def main(loop):
+    reads = 5
+    data = {}
+    for i in range(loop):
+        air_conditioner = False
+        heater = False
+        key = f"dia_{i + 1}"
+        print(f"iniciando o {i + 1}º dia ")
+        if key not in data:
+            data[key] = []
+        records = []
+
+
+
+print("-" * 65)
+print("Seja bem vindo ao simulador de alteração de temperatuas com IOT")
+print(f"    Sistema desenvolvido por {dev_1}, {dev_2} e {gerente}")
+print("-" * 65, "\n")
+looping = int(input("de quantos dias deseja gerar o relátorio? "))
+#main(looping)
 
